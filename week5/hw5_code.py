b03674ce88cb90d22472a5acf016a26c81c67968
ghosalya <gede_ghosalya@mymail.sutd.edu.sg>
2018-06-13 20:09:19 +0800

    coding part done for gudboi (need to do for Senor Shout)

---
+++ week5/hw5_code.py
@@ -0,0 +1,42 @@
+'''
+00 - The Fool 
+
+Fooling classifier code
+for homework 5
+'''
+import torch
+import PIL.Image as Image
+
+import torchvision
+from torchvision import models, transforms, utils
+
+import numpy as np
+import matplotlib.pyplot as plt
+import getimagenetclasses as ginc
+
+model_transform =  transforms.Compose([
+                    transforms.Resize(224),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ])
+
+inv_transform = transforms.Compose([ transforms.Normalize(mean = [ 0., 0., 0. ],
+                                                        std = [ 1/0.229, 1/0.224, 1/0.225 ]),
+                                transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],
+                                                        std = [ 1., 1., 1. ]),
+                                transforms.ToPILImage(),
+                                ])
+
+i2s, s2i, s2d = ginc.parsesynsetwords('synset_words.txt')
+
+def get_descript(class_no):
+    s = i2s[class_no]
+    desc = s2d[s]
+    return desc
+
+def get_trained_resnet(use_gpu=True):
+    model_ft = models.resnet18(pretrained=True)
+    if use_gpu:
+        model_ft = model_ft.cuda(0)
+    return model_ft
